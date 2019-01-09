d6f5c2bf3158bd0c9bb050b1898e05ae1845fc88
yufeng <littlefan_li@163.com>
2019-01-09 12:15:04 +0100

    execute some basic features successfully

--- clean.py
+++ clean.py
@@ -2,18 +2,24 @@
 
 import numpy as np
 import pandas as pd
+from pandas import Series, DataFrame
 
 df = pd.read_csv('task.csv')
+# print(df['ounces'])
+# print(df['ounces'].isnull())
 #remane the columns
 new_name = {'food': 'Food Name', 'ounces': 'Ounces', 'animal': 'Animal'}
 df.rename(columns=new_name, inplace=True)
 print(df)
+print('-----Final output---------')
 #replace the illegal value column of Ounces
-df['Ounces'].replace(-3,3)
-#fill the NaN value in column of Ounces
-df['Ounces'].fillan(df['Ounces'].mean(), inplace=True)
+df.loc[6,'Ounces']=3.0
+# fill the NaN value in column of Ounces
+df['Ounces'].fillna(df['Ounces'].mean(), inplace=True)
+#print(df)
+# convert the first character into uppercase in column of Food Name
+df['Food Name'] = df['Food Name'].str.title()
+df['Animal'] = df['Animal'].str.title()
 print(df)
-#convert the first character into uppercase in column of Food Name
-df['Food Name'].Series.str.capitalize()
-df.to_csv('task_modified.csv')
+#df.to_csv('task_modified.csv')
 
